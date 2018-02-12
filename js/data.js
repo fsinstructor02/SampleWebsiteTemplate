b244c6acbb72b7048bb4a57c042eb4c546c10f89
unknown <fsinstructor02@hussiancollege.edu>
2018-02-12 17:55:40 -0500

    update

--- js/data.js
+++ js/data.js
@@ -1,14 +1,15 @@
-var myObj = [{ 
-    "home":{
-        "navbtn": "home"
-    }, 
-    "resume":{
-        "navbtn": "resume"
-    }, 
-    "portfolio":{
-        "navbtn": "portfolio"
-    }, 
-    "contact": {
-        "navbtn": "contact"
-    }
- }];
+// var myObj = [{ 
+//     "home":{
+//         "navbtn": "home"
+//     }, 
+//     "resume":{
+//         "navbtn": "resume"
+//     }, 
+//     "portfolio":{
+//         "navbtn": "portfolio"
+//     }, 
+//     "contact": {
+//         "navbtn": "contact"
+//     }
+//  }];
+
