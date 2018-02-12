1eb69d2c6519f3f32c52d2db64879651fe7fc06e
unknown <fsinstructor02@hussiancollege.edu>
2018-02-12 00:05:01 -0500

    js update

--- js/main.js
+++ js/main.js
@@ -2,17 +2,19 @@
 
 jQuery(function(){ // JQuery function call to run code
 
-    // $.getJSON("js/data.js", function(result){
-    //     $.each(result, function(i, field){
-    //         $("body").append(field + " ");
-    //     });
-    // });
-
     for(i = 0; i < myObj.length; i++) { 
         
+
+        // Homepage Data
         var homeBtn = myObj[i]["home"]["navbtn"];
+
+        // Portfolio Page Data
         var portfolioBtn = myObj[i]["portfolio"]["navbtn"];
+
+        // Resume Page Data
         var resumeBtn = myObj[i]["resume"]["navbtn"];
+
+        // Contact Page Data
         var contactBtn = myObj[i]["contact"]["navbtn"];
 
         console.log(homeBtn);
